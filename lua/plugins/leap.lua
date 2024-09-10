c6fe9fdb2d50dbfc1eff5f12c704c9f3960c6eac
Gaveen Prabhasara <gaveen@pm.me>
2024-09-10 18:39:34 +0530

    Add leap motion tweaks

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,6 +1,11 @@
 return {
     "ggandor/leap.nvim",
     opts = function()
+        -- uncomment next line to remap 's' and 'S' which can be problematic
         -- require('leap').create_default_mappings()
+        -- additional equivalence classes
+        require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+        -- use the traversal keys to repeat the previous motion
+        require('leap.user').set_repeat_keys('<enter>', '<backspace>')
     end,
 }
