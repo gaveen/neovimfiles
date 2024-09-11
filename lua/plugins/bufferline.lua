4a90eb30872dd1f539f99f54116f68e3db2a6c16
Gaveen Prabhasara <gaveen@pm.me>
2024-09-11 18:01:41 +0530

    Tweak bufferline for less distractions

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,7 +6,39 @@ return {
         local bufferline = require('bufferline')
         bufferline.setup({
             options = {
+                themable = true,
+                numbers = 'buffer_id',
                 always_show_bufferline = false,
+                show_buffer_icons = false,
+                offsets = {
+                    {
+                        filetype = "NvimTree",
+                        text = " [File Browser]",
+                        text_align = "left",
+                        separator = false,
+                    }
+                },
+            },
+            highlights = {
+                buffer_selected = {
+                    bold = false,
+                    italic = false,
+                },
+                buffer_visible = {
+                    bold = false,
+                    italic = false,
+                },
+                numbers_selected = {
+                    bold = false,
+                    italic = false,
+                },
+                numbers_visible = {
+                    bold = false,
+                    italic = false,
+                },
+                fill = {
+                    bg = '#24273a',
+                },
             },
         })
     end,
