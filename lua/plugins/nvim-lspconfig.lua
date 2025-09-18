c2ae46f1ad52b74050a7fd439a46692f04e8ea6d
Gaveen Prabhasara <gaveen@pm.me>
2025-09-18 15:41:32 +0530

    Update nvim-lspconfig to avoid deprecated config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,44 +1,41 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
-
-        local lspconfig = require'lspconfig'
-
         -- for Deno
-        lspconfig.denols.setup {
-          on_attach = on_attach,
-          root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-        }
-
+        vim.lsp.config('denols', {
+            on_attach = on_attach,
+            root_markers = {"deno.json", "deno.jsonc"},
+        })
+        
         -- for TypeScript
-        lspconfig.ts_ls.setup {
-          on_attach = on_attach,
-          root_dir = lspconfig.util.root_pattern("package.json"),
-          single_file_support = false
-        }
-
+        vim.lsp.config('ts_ls', {
+            on_attach = on_attach,
+            root_markers = {"package.json"},
+            single_file_support = false,
+        })
+        
         -- for Rust: rust_analyzer config when not using rustaceanvim
-        -- lspconfig.rust_analyzer.setup({
-        --     on_attach = on_attach,
-        --     settings = {
-        --         ["rust-analyzer"] = {
-        --             imports = {
-        --                 granularity = {
-        --                     group = "module",
-        --                 },
-        --                 prefix = "self",
-        --             },
-        --             cargo = {
-        --                 buildScripts = {
-        --                     enable = true,
-        --                 },
-        --             },
-        --             procMacro = {
-        --                 enable = true,
-        --             },
-        --         },
-        --     },
-        -- })
+        --vim.lsp.config('rust_analyzer', {
+        --    on_attach = on_attach,
+        --    settings = {
+        --        ["rust-analyzer"] = {
+        --            imports = {
+        --                granularity = {
+        --                    group = "module",
+        --                },
+        --                prefix = "self",
+        --            },
+        --            cargo = {
+        --                buildScripts = {
+        --                    enable = true,
+        --                },
+        --            },
+        --            procMacro = {
+        --                enable = true,
+        --            },
+        --        },
+        --    },
+        --})
     end,
     opts = {
         setup = {
