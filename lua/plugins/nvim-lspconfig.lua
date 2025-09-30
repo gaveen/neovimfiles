a1b3c35ac7889e737999252bae7cd4d279dfe4dc
Gaveen Prabhasara <gaveen@pm.me>
2025-09-30 13:43:24 +0530

    Add lspconfig for tofu_ls

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,19 +1,28 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
+        -- for OpenTofu (Terraform)
+        vim.lsp.config['tofu_ls'] = {
+          cmd = { 'tofu-ls', 'serve' },
+          -- Base filetypes
+          filetypes = { 'terraform', 'terraform-vars' },
+          root_markers = {'.terraform', '.git'},
+        }
+        vim.lsp.enable('tofu_ls')
+
         -- for Deno
         vim.lsp.config('denols', {
             on_attach = on_attach,
             root_markers = {"deno.json", "deno.jsonc"},
         })
-        
+
         -- for TypeScript
         vim.lsp.config('ts_ls', {
             on_attach = on_attach,
             root_markers = {"package.json"},
             single_file_support = false,
         })
-        
+
         -- for Rust: rust_analyzer config when not using rustaceanvim
         --vim.lsp.config('rust_analyzer', {
         --    on_attach = on_attach,
